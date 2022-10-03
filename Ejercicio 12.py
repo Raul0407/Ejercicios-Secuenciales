7b80fd16d9a8f4339d0aded052e545b7a41834e8
Raul <al_rlopezm@iessierradelasvillas.org>
2022-10-03 20:42:46 +0200

    Ejercicio 12-13 Terminados en Casa

--- Ejercicio 12.py
+++ Ejercicio 12.py
@@ -1,6 +1,10 @@
 #EJERCICIO 12
 
+import math
 x1 = (int)(input("Dime la distancia de x1:\n"))
 y1 = (int)(input("Dime la distancia de y1:\n"))
 x2 = (int)(input("Dime la distancia de x2:\n"))
 y2 = (int)(input("Dime la distancia de y2:\n"))
+distanciax = abs(x1-x2)
+distanciay = abs(y1-y2)
+print("La distancia entre las x es:", distanciax, "y la distancia entre las y es:", distanciay)
--- Ejercicio 13.py
+++ Ejercicio 13.py
@@ -1,2 +1,6 @@
 #EJERCICIO 13
 
+import math
+numero = (int)(input("Dime un número: "))
+raiz_cubica = numero**(1/3)
+print("Su raiz cuadrada es:", math.sqrt(numero), "y su raiz cúbica es:", raiz_cubica)
