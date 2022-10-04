5fa549701e65d62d7f78747cd066371cfab3c90c
Raul <al_rlopezm@iessierradelasvillas.org>
2022-10-04 14:21:31 +0200

    Programa Spiderman Actualizado

--- Programa Spiderman.py
+++ Programa Spiderman.py
@@ -4,7 +4,9 @@ distancia1 = (int)(input("Dime la distancia 1:\n"))
 distancia2 = (int)(input("Dime la distancia 2:\n"))
 if abs(distancia1)>abs(distancia2):
     print("Primero deberás de ir al destino 2")
+    distanciat = abs(distancia1)+abs(distancia2)*2
+    print("La distancia recorrida sería:", (distanciat))
 else:
     print("Primero deberás de ir al destino 1")
-distanciat = abs(distancia1)*2+abs(distancia2)
-print("La distancia recorrida sería:", (distanciat))
+    distanciat = abs(distancia1)*2+abs(distancia2)
+    print("La distancia recorrida sería:", (distanciat))
