fbb07e243fd6c40a315f2151bf2c08dfd5f00372
Raul <al_rlopezm@iessierradelasvillas.org>
2022-10-04 14:18:40 +0200

    Programa Spiderman

--- Ejercicio 15.py
+++ Ejercicio 15.py
@@ -1,2 +1,4 @@
 #EJERCICIO 15
 
+numero1 = (int)(input("Dime el valor de la variable A:\n"))
+numero2 = (int)(input("Dime el valor de la variable B:\n"))
---
+++ Programa Spiderman.py
@@ -0,0 +1,10 @@
+#PROGRAMA SPIDERMAN
+
+distancia1 = (int)(input("Dime la distancia 1:\n"))
+distancia2 = (int)(input("Dime la distancia 2:\n"))
+if abs(distancia1)>abs(distancia2):
+    print("Primero deberás de ir al destino 2")
+else:
+    print("Primero deberás de ir al destino 1")
+distanciat = abs(distancia1)*2+abs(distancia2)
+print("La distancia recorrida sería:", (distanciat))
