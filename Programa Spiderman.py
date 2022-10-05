01d9fafaa8186bc049deae6d2bd5d82b1a2f8fd5
Raul <al_rlopezm@iessierradelasvillas.org>
2022-10-05 22:27:54 +0200

    Reto Spiderman Actualizado y Funcional

--- Programa Spiderman.py
+++ Programa Spiderman.py
@@ -3,10 +3,30 @@
 distancia1 = (int)(input("Dime la distancia 1:\n"))
 distancia2 = (int)(input("Dime la distancia 2:\n"))
 if abs(distancia1)>abs(distancia2):
-    print("Primero deberás de ir al destino 2")
-    distanciat = abs(distancia1)+abs(distancia2)*2
-    print("La distancia recorrida sería:", (distanciat))
+    if distancia1>0 and distancia2>0:
+        print("Primero deberás de ir al destino 2")
+        distanciat = abs(distancia1)
+        print("La distancia recorrida sería:", (distanciat))
+    else:
+        if distancia1<0 and distancia2<0:
+            print("Primero deberás de ir al destino 2")
+            distanciat = abs(distancia1)
+            print("La distancia recorrida sería:", (distanciat))
+        else:
+            print("Primero deberás de ir al destino 2")
+            distanciat = abs(distancia1)+abs(distancia2)*2
+            print("La distancia recorrida sería:", (distanciat))
 else:
-    print("Primero deberás de ir al destino 1")
-    distanciat = abs(distancia1)*2+abs(distancia2)
-    print("La distancia recorrida sería:", (distanciat))
+    if distancia1>0 and distancia2>0:
+        print("Primero deberás de ir al destino 1")
+        distanciat = abs(distancia2)
+        print("La distancia recorrida sería:", (distanciat))
+    else:
+        if distancia1<0 and distancia2<0:
+            print("Primero deberás de ir al destino 1")
+            distanciat = abs(distancia2)
+            print("La distancia recorrida sería:", (distanciat))
+        else:
+            print("Primero deberás de ir al destino 1")
+            distanciat = abs(distancia1)*2+abs(distancia2)
+            print("La distancia recorrida sería:", (distanciat))
