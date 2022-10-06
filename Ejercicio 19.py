31d5142a111bda2acc2e6fa9993fb5830d43c078
Raul <al_rlopezm@iessierradelasvillas.org>
2022-10-06 18:17:46 +0200

    Ejercicio 19 Hecho (Falta el 17 y 18)

--- Ejercicio 19.py
+++ Ejercicio 19.py
@@ -1,6 +1,11 @@
 #EJERCICIO 19
 
 print("El examen es de 20 preguntas")
-correctas = (int)("Dime cuantas respuestas correctas has tenido")
-incorrectas = (int)("Dime cuantas respuestas incorrectas has tenido")
-blanco = (int)("Dime cuantas respuestas incorrectas has tenido")
+correctas = (int)(input("Dime cuantas respuestas correctas has tenido:\n"))
+incorrectas = (int)(input("Dime cuantas respuestas incorrectas has tenido:\n"))
+blanco = (int)(input("Dime cuantas respuestas en blanco has tenido:\n"))
+if correctas+incorrectas+blanco==20:
+    resultado = (correctas*5)+(incorrectas*-1)+(blanco*0)
+    print("Has sacado un total de:", resultado, "puntos sobre 100")
+else:
+    print("Tus respuestas no suman las 20 preguntas")
