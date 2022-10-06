603465e49bb198192a1d64283a58e4cd6fa8f28e
Raul <al_rlopezm@iessierradelasvillas.org>
2022-10-06 10:15:59 +0200

    Ejercicio 15 y 20 Terminados

--- Ejercicio 16.py
+++ Ejercicio 16.py
@@ -1,5 +1,22 @@
 #EJERCICIO 16
 
-v1 = (int)(print("Dime la velocidad del vehículo 1 (en km/h):\n"))
-v2 = (int)(print("Dime la velocidad del vehículo 2 (en km/h):\n"))
-distancia = (int)(input("Dime la distancia entre ellos (en km):\n"))
+v1 = (int)(input("Dime la velocidad del vehículo 1 (en km/h):\n"))
+v1enmetros = v1*1000/3600
+v2 = (int)(input("Dime la velocidad del vehículo 2 (en km/h):\n"))
+v2enmetros = v2*1000/3600
+if v1enmetros<v2enmetros:
+    print("Nunca se encontrarán, porque el coche 1 va más lento")
+else:
+    if v1enmetros == v2enmetros:
+        print("Nunca se encontrarán, porque los 2 coches van a la misma velocidad")
+    else:
+        distancia = (int)(input("Dime la distancia entre ellos (en km):\n"))
+        posicioncoche1 = 0
+        distanciaenmetros = distancia*1000
+        posicioncoche2 = distanciaenmetros
+
+            #Fórmula = posicionfinalcoche1=posicioninicialcoche1*v1(t)
+            #Fórmula2 = posicionfinalcoche1=posicioninicialcoche2*v2(t)
+
+        tiempo = posicioncoche2/(v1enmetros+v2enmetros)
+        print("Tardarían en encontrarse:", tiempo, "minutos")
--- Ejercicio 20.py
+++ Ejercicio 20.py
@@ -1,2 +1,11 @@
 #EJERCICIO 20
 
+doseuros = (int)(input("Dime cuantas monedas tienes de 2 euros:\n"))
+uneuro = (int)(input("Dime cuantas monedas tienes de 1 euro:\n"))
+cincuentacentimos = (int)(input("Dime cuantas monedas tienes de 50 centimos:\n"))
+veintecentimos = (int)(input("Dime cuantas monedas tienes de 20 centimos:\n"))
+diezcentimos = (int)(input("Dime cuantas monedas tienes de 10 centimos:\n"))
+monedaseuros = doseuros+uneuro
+centimos = cincuentacentimos*0.5+veintecentimos*0.2+diezcentimos*0.10
+dinerototal = monedaseuros+centimos
+print("En total, tienes:", dinerototal, "euros")
