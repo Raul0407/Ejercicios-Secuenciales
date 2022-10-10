c429573eeae2ef1e9dbedb303cf17f61ec505eec
Raul <al_rlopezm@iessierradelasvillas.org>
2022-10-10 11:17:01 +0200

    Agenda de Teléfonos + Ejercicio 17

--- Ejercicio 17.py
+++ Ejercicio 17.py
@@ -1,2 +1,8 @@
 #EJERCICIO 17
 
+horasinicio = (int)(input("¿A qué hora has salido?:\n"))
+minutosinicio = (int)(input("¿En qué minuto has salido?:\n"))
+segundosinicio = (int)(input("¿En qué segundo has salido?:\n"))
+tiempodeltrayecto = (int)(input("¿Cuánto tiempo has tardado (en segundos)?"))
+tiempototalinicio = horasinicio*3600+minutosinicio*60+segundosinicio
+tiempofinal = tiempototalinicio + tiempodeltrayecto
--- Ejercicio 20.py
+++ Ejercicio 20.py
@@ -5,7 +5,8 @@ uneuro = (int)(input("Dime cuantas monedas tienes de 1 euro:\n"))
 cincuentacentimos = (int)(input("Dime cuantas monedas tienes de 50 centimos:\n"))
 veintecentimos = (int)(input("Dime cuantas monedas tienes de 20 centimos:\n"))
 diezcentimos = (int)(input("Dime cuantas monedas tienes de 10 centimos:\n"))
-monedaseuros = doseuros+uneuro
+doseurosb = doseuros*2
+monedaseuros = doseurosb+uneuro
 centimos = cincuentacentimos*0.5+veintecentimos*0.2+diezcentimos*0.10
 dinerototal = monedaseuros+centimos
 print("En total, tienes:", dinerototal, "euros")
