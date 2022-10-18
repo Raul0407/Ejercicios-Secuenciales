b98de3c849050c8347c6a75725b09c1f50b16d50
Raul <al_rlopezm@iessierradelasvillas.org>
2022-10-18 14:40:41 +0200

    Ejercicio 15 + Agenda de Teléfonos

--- Retos/Agenda de teléfonos.py
+++ Retos/Agenda de teléfonos.py
@@ -12,20 +12,24 @@ Ejemplo: [Juan - Teléfono, Pepe - Teléfono]
 
 #Opción 1
 
-vNombres = []
-vTelefonos = []
-opcion = 0
-while opcion != 5:
-    print("-----Menú Agenda-----")
-    print("1-Insertar Contácto")
-    print("2-Borrar Contácto")
-    print("3-Buscar Contácto")
-    print("4-Ver todos los Contáctos")
-    print("5-Salir")
-    try:
-        opcion = (int)(input("Introduce una opción:\n"))
-    except:
-        print("Las opciones son de la 1 a la 5")
+def pintaMenu(opc:int):
+    vNombres = []
+    vTelefonos = []
+    opcion = 0
+    while opcion < 5 or opcion>5:
+        print("-----Menú Agenda-----")
+        print("1-Insertar Contácto")
+        print("2-Borrar Contácto")
+        print("3-Buscar Contácto")
+        print("4-Ver todos los Contáctos")
+        print("5-Salir")
+        try:
+            opcion = (int)(input("Introduce una opción:\n"))
+        except:
+            print("Las opciones son de la 1 a la 5")
+    return opcion
     
-    
-print("Saliste de la base de datos")
+    print("Saliste de la base de datos")
+
+pintaMenu("Has seleccionado", pintaMenu())
+
--- Serie 2/Ejercicio 15 Serie 2.py
+++ Serie 2/Ejercicio 15 Serie 2.py
@@ -1,3 +1,9 @@
 #EJERCICIO 15
 
 alumnos = (int)(input("Dime la cantidad de alumnos qque irán al viaje:\n"))
+if alumnos>=100:
+    print("El costo por alumno sería de 65 euros")
+if alumnos>=50 and alumnos<100:
+    print("El costo por alumno sería de 70 euros")
+if alumnos>=30 and alumnos<50:
+    print("El costo por alumno sería de 95 euros")
