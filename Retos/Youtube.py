74ac02f0b059df2add51518b7c4c41e647a678cc
Raul <al_rlopezm@iessierradelasvillas.org>
2022-10-27 10:13:06 +0200

    Descargar listas de Youtube

--- Retos/Youtube.py
+++ Retos/Youtube.py
@@ -1,6 +1,7 @@
 #Descargador de videos de Youtube
 
 from pytube import YouTube
+from pytube import Playlist
 
 ''' Opción de Juanfran
 print("Empezando")
@@ -15,7 +16,8 @@ print("Descargando")
 mp3.download()
 '''
 
-
+'''
+Descargar una canción
 def descargaCancion(url:str):
     youtube = YouTube(url)
     print(youtube.author)
@@ -24,3 +26,14 @@ def descargaCancion(url:str):
     cancion.download()
 
 descargaCancion("https://www.youtube.com/watch?v=YVkUvmDQ3HY")
+'''
+
+def descargarPlaylist(url:str):
+    playlist = Playlist(url)
+    for cancion in playlist.videos:
+        print("Descargando canción:", cancion.title)
+        cancion.streams.get_audio_only().download("Ejercicios-Secuenciales/Canciones/")
+        
+
+url = "https://www.youtube.com/playlist?list=PLSFitF4B6yNS82pcRx5XvD1PB6m8lIs5J"
+descargarPlaylist("https://www.youtube.com/playlist?list=PLSFitF4B6yNS82pcRx5XvD1PB6m8lIs5J")
