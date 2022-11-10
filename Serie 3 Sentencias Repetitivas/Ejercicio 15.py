db385011e0d7561f429cea3a331cb63740480b22
Raul <al_rlopezm@iessierradelasvillas.org>
2022-11-10 10:05:38 +0100

    Ejercicio 15 Actualizado

--- Serie 3 Sentencias Repetitivas/Ejercicio 15.py
+++ Serie 3 Sentencias Repetitivas/Ejercicio 15.py
@@ -3,6 +3,7 @@
 pagototal = 0
 primerpago = 10
 for mes in range(1, 21):
+	print("Este mes pagó:", primerpago, "euros")	
 	pagototal = pagototal + primerpago
 	primerpago = primerpago * 2
 print("En total, tras los 20 meses paga un total de: ",pagototal, "euros")
