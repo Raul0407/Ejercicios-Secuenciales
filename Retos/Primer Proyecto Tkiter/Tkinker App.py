4cf062e16b8e39c6614c4fbd99d4eddfdd404b2b
Raul <al_rlopezm@iessierradelasvillas.org>
2022-11-18 11:15:09 +0100

    Proyecto Tkinter

---
+++ Retos/Primer Proyecto Tkiter/Tkinker App.py
@@ -0,0 +1,21 @@
+from tkinter import *
+from tkinter import ttk
+
+#GENERO LA VENTANA
+ventana = Tk()
+ventana.title("Primer Ejercicio")
+ventana.geometry("250x300")
+ventana.resizable()
+ventana.config(background="gold")
+ventana.iconbitmap("images.ico")
+
+#GENERO EL LIENZO PARA PINTAR COMPONENTES
+frm = ttk.Frame(ventana).pack() #o .pack
+
+#COMPONENTES LABEL Y BUTTOM
+labelTexto = ttk.Label(frm, text="Hola Tkinter") #o .pack
+labelTexto.config(background="light blue", border=5, font=("Arial", 15))
+labelTexto.pack()
+
+ttk.Button(ventana, text="Salir", command=ventana.destroy).pack() #o .pack
+ventana.mainloop()
