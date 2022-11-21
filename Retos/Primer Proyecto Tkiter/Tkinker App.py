1b781fd6d2d7dff01aa4327f730a27e9bb93851b
Raul <al_rlopezm@iessierradelasvillas.org>
2022-11-21 11:15:27 +0100

    Proyecto Tkinter 2.0

--- Retos/Primer Proyecto Tkiter/Tkinker App.py
+++ Retos/Primer Proyecto Tkiter/Tkinker App.py
@@ -1,21 +1,35 @@
 from tkinter import *
 from tkinter import ttk
+import tkinter as tk
+
+def Saludar():
+    texto = campoTexto.get()
+    textoLabel.set(texto)
 
 #GENERO LA VENTANA
 ventana = Tk()
 ventana.title("Primer Ejercicio")
 ventana.geometry("250x300")
 ventana.resizable()
-ventana.config(background="gold")
-ventana.iconbitmap("images.ico")
+ventana.config(background="deep sky blue")
+icono = tk.PhotoImage(file="images.png")
+ventana.iconbitmap(icono)
 
 #GENERO EL LIENZO PARA PINTAR COMPONENTES
 frm = ttk.Frame(ventana).pack() #o .pack
 
 #COMPONENTES LABEL Y BUTTOM
-labelTexto = ttk.Label(frm, text="Hola Tkinter") #o .pack
-labelTexto.config(background="light blue", border=5, font=("Arial", 15))
+textoLabel = StringVar()
+textoLabel.set("Hola Tkinter")
+labelTexto = ttk.Label(frm, textvariable=textoLabel) #o .pack
+labelTexto.config(background="gold", border=5, font=("Arial", 15))
 labelTexto.pack()
 
+#COMPONENTE CUADRO DE TEXTO
+campoTexto = ttk.Entry(frm)
+campoTexto.pack()
+
+ttk.Button(ventana, text="Te digo lo que escribes", command = Saludar).pack() #o .pack
 ttk.Button(ventana, text="Salir", command=ventana.destroy).pack() #o .pack
+
 ventana.mainloop()
