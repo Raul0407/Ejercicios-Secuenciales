6d4f19250df55c59a961ddd1c678e2e61241db45
Raul <al_rlopezm@iessierradelasvillas.org>
2022-12-12 11:15:20 +0100

    Nuevo Proyecto

--- Python/Projects/Retos/Flet/Primer Proyecto/Primer Proyecto.py
+++ Python/Projects/Retos/Flet/Primer Proyecto/Primer Proyecto.py
@@ -1,7 +1,7 @@
 import flet as ft
 
 def main(page: ft.Page):
-
+    page.title="Flet inicio"
     def cambiarColor(e):
         for i in range(11):
             text = ft.Text(value = f"Texto número {i}", size = 12.5)
@@ -17,7 +17,19 @@ def main(page: ft.Page):
     #COMPONENTE BOTÖN
     boton = ft.FloatingActionButton(icon = ft.icons.ADD, on_click=cambiarColor)
     page.add(boton)
-
     page.update()
+    
+    textField_Nombre = ft.TextField(label="Nombre",hint_text="Escribe tu nombre")
+    page.add(textField_Nombre)
+    dropDown_Menu = ft.Dropdown(width=300,options=[ft.dropdown.Option("Hombre")])
+    page.add(dropDown_Menu)
+    dropDown_Menu.options.append(ft.dropdown.Option("Mujer"))
+    page.update()
+
+    slider_edad=ft.Slider(min=0,max=120,divisions=120,label="Edad{value}")
+    page.add(slider_edad)
+    #Crear fila
+    fila = ft.Row(controls=[textField_Nombre,dropDown_Menu])
+    page.add(fila)
 
 ft.app(target = main)
--- Python/Projects/Retos/Flet/Segundo Proyecto/Segundo Proyecto.py
+++ Python/Projects/Retos/Flet/Segundo Proyecto/Segundo Proyecto.py
@@ -1,1 +1,20 @@
-import flet as f
+import flet as ft
+
+def guardarDatos(textField_Alimentos):
+    vlista = []
+    vlista.append(textField_Alimentos)
+    print(vlista)
+
+def main(page: ft.Page):
+    page.title="Flet inicio"
+    texto = ft.Text(value = "Lista de la Compra", color = "blue", size = 25)
+    page.add(texto)
+    
+    textField_Alimentos = ft.TextField(label="Ingredientes",hint_text="Escribe tu lista de la compra (Pulsa el botón para guardar)")
+    page.add(textField_Alimentos)
+
+    boton = ft.FloatingActionButton(icon = ft.icons.ADD, on_click = guardarDatos)
+    page.add(boton)
+    page.update()
+
+ft.app(target = main)
