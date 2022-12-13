1509206a92b0927e9f257fe0d98464a74145d73b
Raul <al_rlopezm@iessierradelasvillas.org>
2022-12-13 14:40:31 +0100

    Proyecto 2 Actualizado

--- Python/Projects/Retos/Flet/Segundo Proyecto/Segundo Proyecto.py
+++ Python/Projects/Retos/Flet/Segundo Proyecto/Segundo Proyecto.py
@@ -1,20 +1,35 @@
 import flet as ft
 
-def guardarDatos(textField_Alimentos):
-    vlista = []
-    vlista.append(textField_Alimentos)
-    print(vlista)
+vlista = []
+
+
+
 
 def main(page: ft.Page):
+
+    def guardarDatos(e):
+        
+        vlista.append(textField_Alimentos.value)
+        print(vlista)
+
+
+
+
     page.title="Flet inicio"
+    page.scroll = "adaptive"
     texto = ft.Text(value = "Lista de la Compra", color = "blue", size = 25)
     page.add(texto)
     
-    textField_Alimentos = ft.TextField(label="Ingredientes",hint_text="Escribe tu lista de la compra (Pulsa el botón para guardar)")
+    textField_Alimentos = ft.TextField(label="Ingredientes",hint_text="Escribe tu lista de la compra (Pulsa el botón + para guardar)")
     page.add(textField_Alimentos)
 
-    boton = ft.FloatingActionButton(icon = ft.icons.ADD, on_click = guardarDatos)
+
+    
+    boton = ft.FloatingActionButton(icon = ft.icons.ADD, on_click = guardarDatos, )
     page.add(boton)
+    boton2 = ft.FloatingActionButton(icon = ft.icons.ABC)
+    page.add(boton2)
     page.update()
+    
 
 ft.app(target = main)
